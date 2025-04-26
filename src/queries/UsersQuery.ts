1a04da1d452a08d8d2fb68c3e568a6a4cf2d2982
LAPTOP-RLVESH4A\Rana <makki_rana@outlook.com>
2025-04-26 19:15:02 -0400

    sample query

---
+++ src/queries/UsersQuery.ts
@@ -0,0 +1,18 @@
+import { graphql } from "react-relay";
+
+export const UsersQuery = graphql`
+  query UsersQuery($results: Int!, $nat: String!) {
+    users(results: $results, nat: $nat) {
+      gender
+      name {
+        last
+      }
+      dob {
+        age
+      }
+      location {
+        state
+      }
+    }
+  }
+`;
