66347f11a48bcdc53a101ab45a2fcf2fa19db42f
LAPTOP-RLVESH4A\Rana <makki_rana@outlook.com>
2025-04-26 18:09:18 -0400

    Added Relay to wrap around app

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { RelayEnvironmentProvider } from 'react-relay/hooks';
+import { relayEnvironment } from './relay/RelayEnvironments';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RelayEnvironmentProvider environment={relayEnvironment}>
+      <App />
+    </RelayEnvironmentProvider>
   </React.StrictMode>
 );
 
